ed4f2fa3da355b4e554edd76edffa865aaadabc0
nithyanand <nithyanandnadar.m@gmail.com>
2025-02-08 21:11:45 +0530

    firstcommit

---
+++ src/app/menu/menu-item/menu-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-item',
+  imports: [],
+  templateUrl: './menu-item.component.html',
+  styleUrl: './menu-item.component.css'
+})
+export class MenuItemComponent {
+
+}
